9a50ee7d3afe875043dfe86e3589b4b56836bd11
nbeckles <nbeckles@gmail.com>
2022-04-28 08:05:37 -0400

    update itemsInCategory variable name to improve code readability

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,25 +1,25 @@
-import { useState } from "react";
 import Item from "./Item";
 
 function Category(props) {
     const name = props.name;
     const id = props.id;
-    // const [filteredInventory, setFilteredInventory] = useState([]);
-    // setFilteredInventory(props.items);
-    const filteredInventory=props.items;
-
-    //use props name to get items from database and url?
-    //or pass filtered array as props?
+    const itemsInCategory=props.items;
 
     return (
         <div className="category" id={id}>
             <h2>{name}</h2>
             <div className="items-container">
                 {
-                filteredInventory.map(item=>{
-                    return(
-                        <Item imageUrl={item.image} title={item.title} price={item.price.toFixed(2)}/>
-                    )
+                itemsInCategory.map(item=>{
+                    return (
+                        <Item
+                            key={item.id}
+                            imageUrl={item.image}
+                            imageAlt={item.title}
+                            title={item.title}
+                            price={item.price.toFixed(2)}
+                        />
+                    );
                 })}
             </div>
         </div>
