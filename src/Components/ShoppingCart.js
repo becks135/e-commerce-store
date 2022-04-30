2582c7168e94f8ec60e12502847eca6350cfd7b6
nbeckles <nbeckles@gmail.com>
2022-04-30 16:38:25 -0400

    add function to get shopping cart items from database

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -4,29 +4,52 @@ import ShoppingCartItem from "./ShoppingCartItem";
 
 //modules
 import {
-  getDatabase,
-  ref,
-  set,
-  onValue,
-  query,
-  orderByChild,
-  equalTo,
-  update,
-  push,
-  remove,
-  get,
+    getDatabase,
+    ref,
+    set,
+    onValue,
+    query,
+    orderByChild,
+    equalTo,
+    update,
+    push,
+    remove,
+    get,
 } from "firebase/database";
 
 //config
 import firebase from "../firebase";
+import { useEffect, useState } from "react";
 
-
-function getShoppingCartItems(){
-     const database = getDatabase(firebase);
-
-}
 
 function ShoppingCart(){
+
+    const [shoppingCartItems, setShoppingCartItems] = useState([]);
+
+    function getShoppingCartItems() {
+        const database = getDatabase(firebase);
+        const shoppingCartDBRef = ref(database, "/shopping-cart");
+        onValue(shoppingCartDBRef,response=>{
+            //shopping cart db ref returns an object.
+            //need to convert the object to an array
+            const cartItems = response.val() 
+            const cartArray = [];
+
+            for(let key in cartItems){
+                cartArray.push(cartItems[key]);
+            }
+
+            //set shoppingCartItems state variables to newly created array containing shopping cart items from database
+            setShoppingCartItems(cartArray);
+        })
+    }
+
+    //TODO: only get shopping cart items when shopping cart displayed
+    useEffect(()=>{
+        getShoppingCartItems();
+    },[])
+   
+
     return (
         <div className="shopping-cart">
             <ul>
