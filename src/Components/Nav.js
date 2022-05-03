4d8266c62e60342728e465f4d8564922e36d6c00
nbeckles <nbeckles@gmail.com>
2022-05-03 19:52:58 -0400

    update nav and shopping cart style

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,14 +4,16 @@ import CartIcon from "./CartIcon";
 function Nav(props){
     return(
         <nav>
-            <a href="#home"><h1>Sahara</h1></a>
-            <ul>
-                <li><a href="#clothing">Clothing</a></li>
-                <li><a href="#jewellery">Jewellery</a></li>
-                <li><a href="#electronics">Electronics</a></li>
-            </ul>
+            <div className="outer-wrapper">
+                <a href="#home"><h1>Sahara</h1></a>
+                <ul>
+                    <li><a href="#clothing">Clothing</a></li>
+                    <li><a href="#jewellery">Jewellery</a></li>
+                    <li><a href="#electronics">Electronics</a></li>
+                </ul>
 
-            <CartIcon cartCount={props.cartCount}/>
+                <CartIcon cartCount={props.cartCount}/>
+            </div>
         </nav>
     )
 }
